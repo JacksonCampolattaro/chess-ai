9e16090b1cf7e299a63d5efacf5136db0e65a9e6
JacksonCampolattaro <jackcamp@vt.edu>
2021-04-09 13:23:18 -0400

    Loop over moves in the first game, and print board states

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import chess.pgn
 
 
 def main():
-    # TODO
-    print("Hello, world")
 
     dataset = open("lichess_db_standard_rated_2013-01.pgn")
     game = chess.pgn.read_game(dataset)
 
-    print(game.board())
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print(game.next().board())
+    board = game.board()
+    for move in game.mainline_moves():
+        board.push(move)
+        print(board)
+        print("~~~~~~~~~~~~~~~")
 
 
 if __name__ == '__main__':
