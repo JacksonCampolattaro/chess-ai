50e19b11381f55648de5b9d5d27c82db62e563ac
mfuhrer@vt.edu <Jc5M9%crNFrT@o8Xnc>
2021-04-09 13:04:16 -0400

    Pseudocode for discussion

--- feature_extractor.py
+++ feature_extractor.py
@@ -2,4 +2,18 @@
 feature_extractor.py
 
 Runs through Lichess dataset and extracts mode label (i.e. most common move) for each feature (board state).
+
+% Record moves in data
+for game in data
+    board = starting board
+    for move in game:
+        move_taken = move
+        record([board, move_taken])
+        board = board+move
+
+for board in boards
+    best_move = mode(moves)
+    record(best_move)
+
+
 """
