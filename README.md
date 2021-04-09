17d8362ea25b46b40862f9c12e9558060077b07c
JacksonCampolattaro <jackcamp@vt.edu>
2021-04-09 13:11:19 -0400

    Add simple demonstration of loading the first game from the dataset

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Using datasets from [Lichess](https://database.lichess.org/)
 
 #### Useful resources:
 * [Using PyTorch with a different dataset format](https://towardsdatascience.com/creating-a-chess-ai-using-deep-learning-d5278ea7dcf)
+* [Python-chess library documentation](https://python-chess.readthedocs.io/en/latest/index.html)
--- main.py
+++ main.py
@@ -6,6 +6,13 @@ def main():
     # TODO
     print("Hello, world")
 
+    dataset = open("lichess_db_standard_rated_2013-01.pgn")
+    game = chess.pgn.read_game(dataset)
+
+    print(game.board())
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print(game.next().board())
+
 
 if __name__ == '__main__':
     main()
